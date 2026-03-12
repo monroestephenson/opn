a5e897cdb442f67a1983c8aec3d5ca63ef36fb8f
Monroe Stephenson <m@gramscian.com>
2026-03-12 10:20:51 +0100

    SHA

--- Formula/opn.rb
+++ Formula/opn.rb
@@ -2,7 +2,7 @@ class Opn < Formula
   desc "Modern, human-friendly replacement for lsof"
   homepage "https://github.com/monroestephenson/opn"
   url "https://github.com/monroestephenson/opn/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "" # update after release: curl -sL <url> | shasum -a 256
+  sha256 "78ea9c4b37264d69dd7e9eede842756d5b67f4a2fe6bb797b20870b29b62e544"
   license "MIT"
 
   depends_on "rust" => :build
